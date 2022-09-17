c730cb595b8641db43edabc12f5387afe105784f
Celie Russier <celie.russier@gmail.com>
2022-09-17 14:46:41 +0200

    POST pour /api/users

--- movieHandlers.js
+++ movieHandlers.js
@@ -57,7 +57,27 @@ const getMovieById = (req, res) => {
     });
 };
 
+const addMovie = (req, res) => {
+    //console.log(req.body);
+    const { title, director, year, color, duration } = req.body;
+
+    database
+    .query(
+      "INSERT INTO movies(title, director, year, color, duration) VALUES (?, ?, ?, ?, ?)",
+      [title, director, year, color, duration]
+    )
+    .then(([result]) => {
+      res.location(`/api/movies/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error saving the movie");
+    });
+    //res.send("Post route is working 🎉");
+  };
+
 module.exports = {
   getMovies,
   getMovieById,
+  addMovie
 };
